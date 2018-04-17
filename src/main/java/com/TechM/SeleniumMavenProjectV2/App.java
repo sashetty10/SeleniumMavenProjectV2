4f24b76ceff944755af853757850a143db409a2d
Sachin Shetty <ss00495172@techmahindra.com>
2018-04-17 12:53:21 +0530

    Modification done

---
+++ src/main/java/com/TechM/SeleniumMavenProjectV2/App.java
@@ -0,0 +1,35 @@
+package com.TechM.SeleniumMavenProjectV2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public WebDriver driver;
+	
+	  @Test
+	  public void openMyFBPage() {
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+	  }
+	  
+	  @BeforeClass
+	  public void beforeClass() {
+		  
+		  System.setProperty("webdriver.ie.driver", "F:\\Saradhi.Seshagiri\\IEDriverServer.exe");
+		  driver = new InternetExplorerDriver();
+		  
+	  }
+	 
+	  @AfterClass
+	  public void afterClass() {
+		  driver.quit();
+	  }
+}
